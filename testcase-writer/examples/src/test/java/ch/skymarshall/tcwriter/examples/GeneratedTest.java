82a6d55c0badf566eb44e61eacf458394af82a93
sebastien.caille@gmail.com <sebastien.caille@gmail.com>
2020-01-11 22:02:26 +0100

    Put back emptied test

--- testcase-writer/examples/src/test/java/ch/skymarshall/tcwriter/examples/GeneratedTest.java
+++ testcase-writer/examples/src/test/java/ch/skymarshall/tcwriter/examples/GeneratedTest.java
@@ -27,6 +27,43 @@ public class GeneratedTest {
 	
 	@Test
 	public void testCase() {
+		// Step 1: As customer, I go on internet and buy a coffee machine of brand "OldSchool" (ISO: yes)
+		ch.skymarshall.tcwriter.examples.api.interfaces.selectors.BuyingLocationSelector var1 = BuyingLocationSelector.onInternet();
+		ch.skymarshall.tcwriter.examples.api.interfaces.dto.TestItem var2 = TestItem.coffeeMachineOfBrand("OldSchool");
+		var2.setISO();
+		customer.buy(var1, var2);
+		
+		// Step 2: As delivery company, I deliver the item
+		deliveryGuy.deliverItem();
+		
+		// Step 3: As customer, I get the delivered package and check that the packaged item is a coffee machine of brand "OldSchool" (ISO: yes)
+		ch.skymarshall.tcwriter.examples.api.interfaces.selectors.PackageDeliverySelector var5 = PackageDeliverySelector.deliveredItem();
+		ch.skymarshall.tcwriter.examples.api.interfaces.dto.TestItem var6 = TestItem.coffeeMachineOfBrand("OldSchool");
+		var6.setISO();
+		customer.checkPackage(var5, var6);
+		
+		// Step 4: As customer, I resell the item for 10$
+		customer.resellOwnedItem(10);
+		
+		// Step 5: As customer, I go in a local shop and buy a tea pot
+		ch.skymarshall.tcwriter.examples.api.interfaces.selectors.BuyingLocationSelector var9 = BuyingLocationSelector.inLocalShop();
+		ch.skymarshall.tcwriter.examples.api.interfaces.dto.TestItem var10 = TestItem.teaPot();
+		customer.buy(var9, var10);
+		
+		// Step 6: As customer, I get the package bought at the shop and check that the packaged item is a tea pot
+		ch.skymarshall.tcwriter.examples.api.interfaces.selectors.PackageDeliverySelector var13 = PackageDeliverySelector.fromShop();
+		ch.skymarshall.tcwriter.examples.api.interfaces.dto.TestItem var14 = TestItem.teaPot();
+		customer.checkPackage(var13, var14);
+		
+		// Step 7: As customer, I resell the item for 10$
+		customer.resellOwnedItem(10);
+		
+		// Step 8: As customer, I find another brand
+		java.lang.String ref8 = customer.findAnotherBrand();
+		
+		// Step 9: As customer, I keep the note "[Value of step 8: MidClass]"
+		customer.keepNote(ref8);
+		
 		
 	}
 

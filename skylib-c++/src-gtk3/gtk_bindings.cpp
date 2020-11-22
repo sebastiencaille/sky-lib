f041ac3bbe7b5ef64420370de3520917dea47d3a
sebastien.caille@gmail.com <sebastien.caille@gmail.com>
2020-11-22 23:41:09 +0100

    Cleanups

--- skylib-c++/src-gtk3/gtk_bindings.cpp
+++ skylib-c++/src-gtk3/gtk_bindings.cpp
@@ -20,15 +20,18 @@
  *      Author: scaille
  */
 #include <iostream>
+
+#include "utils.hh"
 #include "gtk_bindings.hh"
 
 namespace ch_skymarshall::gui::gtk {
 
-entry_binding::entry_binding(Gtk::Entry& _entry) :
-				m_entry(_entry) {
+entry_binding::entry_binding(Gtk::Entry &_entry) :
+		m_entry(_entry) {
 }
 
 entry_binding::~entry_binding() {
+	DESTR_LOG("~entry_binding");
 	remove_component_value_change_listener();
 }
 
@@ -37,10 +40,12 @@ void entry_binding::on_changed_signal() {
 	m_componentLink.lock()->set_value_from_component(get_component(), text);
 }
 
-void entry_binding::add_component_value_change_listener(weak_ptr<component_link<Glib::ustring>> _componentLink) {
+void entry_binding::add_component_value_change_listener(
+		weak_ptr<component_link<Glib::ustring>> _componentLink) {
 	if (!m_connection.connected()) {
 		m_componentLink = _componentLink;
-		m_connection = m_entry.signal_changed().connect(sigc::mem_fun(*this, &entry_binding::on_changed_signal));
+		m_connection = m_entry.signal_changed().connect(
+				sigc::mem_fun(*this, &entry_binding::on_changed_signal));
 	}
 }
 
@@ -48,8 +53,8 @@ void entry_binding::remove_component_value_change_listener() {
 	m_connection.disconnect();
 }
 
-void entry_binding::set_component_value(property& _source, Glib::ustring _value) {
-	cout << _value << endl;
+void entry_binding::set_component_value(property &_source,
+		Glib::ustring _value) {
 	m_entry.set_text(_value);
 }
 
@@ -57,20 +62,22 @@ source_ptr entry_binding::get_component() {
 	return (source_ptr) &m_entry;
 }
 
-label_binding::label_binding(Gtk::Label& _label) :
-				m_label(_label) {
+label_binding::label_binding(Gtk::Label &_label) :
+		m_label(_label) {
 }
 
-label_binding::~label_binding() = default;
+label_binding::~label_binding()
+DESTR_WITH_LOG("~label_binding")
 
-void label_binding::add_component_value_change_listener(weak_ptr<component_link<Glib::ustring>> _componentLink) {
+void label_binding::add_component_value_change_listener(
+		weak_ptr<component_link<Glib::ustring>> _componentLink) {
 }
 
 void label_binding::remove_component_value_change_listener() {
 }
 
-
-void label_binding::set_component_value(property& _source, Glib::ustring _value) {
+void label_binding::set_component_value(property &_source,
+		Glib::ustring _value) {
 	m_label.set_text(_value);
 }
 

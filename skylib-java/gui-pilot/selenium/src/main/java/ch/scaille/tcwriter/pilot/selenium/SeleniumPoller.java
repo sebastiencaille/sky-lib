6847909c9ef2b43684d60cebaa717eb910d726d4
Sébastien Caille <sebastien.caille@secutix.com>
2022-01-11 19:19:31 +0100

    Improve selenium test failure

--- skylib-java/gui-pilot/selenium/src/main/java/ch/scaille/tcwriter/pilot/selenium/SeleniumPoller.java
+++ skylib-java/gui-pilot/selenium/src/main/java/ch/scaille/tcwriter/pilot/selenium/SeleniumPoller.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.TimeoutException;
@@ -14,11 +17,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import ch.scaille.util.helpers.Poller;
+import io.netty.handler.logging.LogLevel;
 
 public class SeleniumPoller extends Poller {
 
+	private Logger LOGGER = Logger.getLogger(SeleniumPoller.class.getName());
+
 	private WebDriver webDriver;
 	private TimeoutException timeoutException;
+	private Object lastResult;
 
 	public SeleniumPoller(WebDriver webDriver, Duration timeout, Duration firstDelay, DelayFunction delayFunction) {
 		super(timeout, firstDelay, delayFunction);
@@ -29,8 +36,10 @@ public class SeleniumPoller extends Poller {
 			Function<TimeoutException, T> timeoutHandler) {
 		timeoutException = null;
 		T result = run(polling, isSuccess);
-		if (timeoutException != null) {
-			return timeoutHandler.apply(timeoutException);
+		if (timeoutException != null && lastResult != null) {
+			return (T) lastResult;
+		} else if (timeoutException != null) {
+			timeoutHandler.apply(timeoutException);
 		}
 		return result;
 	}
@@ -45,6 +54,7 @@ public class SeleniumPoller extends Poller {
 			}
 			return pollWithSpecificDelay(polling, isSuccess, delayFunction.apply(this));
 		} catch (TimeoutException e) {
+			LOGGER.log(Level.INFO, "Polling timeout", e);
 			timeoutException = e;
 			return null;
 		}
@@ -57,6 +67,7 @@ public class SeleniumPoller extends Poller {
 						ElementNotInteractableException.class, UnhandledAlertException.class))
 				.<T>until(d -> {
 					T result = polling.apply(this);
+					lastResult = result;
 					if (!isSuccess.test(result)) {
 						return null;
 					}

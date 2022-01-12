dd8780d906877cf7a18669ad4db9f5ef0c195f44
Sébastien Caille <sebastien.caille@secutix.com>
2022-01-12 13:31:02 +0100

    Move test actors definition into @TCActors

---
+++ testcase-writer/annotations/src/main/java/ch/scaille/tcwriter/annotations/TCActors.java
@@ -0,0 +1,18 @@
+package ch.scaille.tcwriter.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface TCActors {
+
+	/**
+	 * List of code|role_simple_call_name|description|human_readable
+	 * @return
+	 */
+	String[] value();
+
+}

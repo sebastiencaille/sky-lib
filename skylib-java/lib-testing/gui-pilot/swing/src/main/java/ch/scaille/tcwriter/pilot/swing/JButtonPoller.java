33756b1a0ff59a96e634b588bc7c27ba29b96e76
sebastien.caille@gmail.com <sebastien.caille@gmail.com>
2024-11-23 15:03:18 +0100

    Use polling builders to poll swing components

---
+++ skylib-java/lib-testing/gui-pilot/swing/src/main/java/ch/scaille/tcwriter/pilot/swing/JButtonPoller.java
@@ -0,0 +1,32 @@
+package ch.scaille.tcwriter.pilot.swing;
+
+import javax.swing.JButton;
+
+public class JButtonPoller extends SwingPollingBuilder<JButton, JButtonPoller, JButtonPoller.SwingPoller> {
+
+	public static class SwingPoller extends ch.scaille.tcwriter.pilot.swing.SwingPollingBuilder.SwingPoller<JButton> {
+
+		protected SwingPoller(JButtonPoller builder) {
+			super(builder);
+		}
+
+		public void click() {
+			configure(polling -> polling.withReportText("click")).applied(JButton::doClick);
+		}
+
+	}
+
+	public JButtonPoller(SwingPilot pilot, String name) {
+		super(new SwingComponentPilot<>(pilot, JButton.class, name));
+	}
+
+	@Override
+	public SwingPoller ifNot() {
+		return new SwingPoller(this);
+	}
+
+	public void click() {
+		fail().ifNot().click();
+	}
+
+}
---
+++ skylib-java/lib-testing/gui-pilot/swing/src/main/java/ch/scaille/tcwriter/pilot/swing/JListPoller.java
@@ -0,0 +1,76 @@
+package ch.scaille.tcwriter.pilot.swing;
+
+import static ch.scaille.tcwriter.pilot.factories.PollingResults.failure;
+import static ch.scaille.tcwriter.pilot.factories.PollingResults.success;
+import static ch.scaille.tcwriter.pilot.factories.Reporting.checkingValue;
+
+import javax.swing.JList;
+
+import org.junit.jupiter.api.Assertions;
+
+import ch.scaille.tcwriter.pilot.Polling;
+
+public class JListPoller extends SwingPollingBuilder<JList, JListPoller, JListPoller.SwingPoller> {
+
+	public static class SwingPoller extends ch.scaille.tcwriter.pilot.swing.SwingPollingBuilder.SwingPoller<JList> {
+
+		protected SwingPoller(JListPoller builder) {
+			super(builder);
+		}
+
+		/**
+		 * Select a value in a list, according to its String representation
+		 */
+		public void select(final String value) {
+			if (value == null) {
+				return;
+			}
+			configure(polling -> polling.withReportText("selecting element " + value)).appliedCtxt(ctxt -> {
+				final var c = ctxt.getComponent();
+				for (int i = 0; i < c.getModel().getSize(); i++) {
+					if (value.equals(c.getModel().getElementAt(i).toString())) {
+						c.setSelectedIndex(i);
+					}
+				}
+				Assertions.assertTrue(ctxt.getPilot().getCachedElement().map(JList::getSelectedIndex).orElse(-1) >= 0,
+						() -> ctxt.getComponent().getName() + ": element must have been selected: " + value);
+			});
+		}
+
+		public void assertSelected(final String expected) {
+			if (expected == null) {
+				return;
+			}
+			configure(polling -> polling.withReportText(checkingValue(expected)))
+					.satisfied(new Polling<>(ctxt -> ctxt.getPilot().canCheck(ctxt), ctxt -> {
+						final var component = ctxt.getComponent();
+						if (component.getSelectedIndex() < 0) {
+							return failure("No element selected");
+						}
+						final var current = component.getModel().getElementAt(component.getSelectedIndex()).toString();
+						if (!expected.equals(current)) {
+							return failure("Wrong element selected (" + current + ")");
+						}
+						return success();
+					}));
+		}
+
+	}
+
+	public JListPoller(SwingPilot pilot, String name) {
+		super(new SwingComponentPilot<>(pilot, JList.class, name));
+	}
+
+	@Override
+	public SwingPoller ifNot() {
+		return new SwingPoller(this);
+	}
+
+	public void select(final String value) {
+		fail().ifNot().select(value);
+	}
+
+	public void assertSelected(final String expected) {
+		fail().ifNot().assertSelected(expected);
+	}
+}

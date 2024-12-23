28cb3cef52746367d806aca69f9e16d436794cbc
sebastien.caille@gmail.com <sebastien.caille@gmail.com>
2024-12-23 19:56:28 +0100

    Cleanups

--- testcase-writer/gui/src/main/java/ch/scaille/tcwriter/gui/frame/ExceptionHelper.java
+++ testcase-writer/gui/src/main/java/ch/scaille/tcwriter/gui/frame/ExceptionHelper.java
@@ -9,6 +9,10 @@ import ch.scaille.util.helpers.Logs;
 
 public class ExceptionHelper {
 	
+	private ExceptionHelper() {
+		// noop
+	}
+	
 	public static void handleException(Component parent, final Exception ex) {
 		Logs.of(ExceptionHelper.class).log(Level.WARNING, "Unable to execute action", ex);
 		JOptionPane.showMessageDialog(parent, "Unable to execution action: " + ex.getMessage());
